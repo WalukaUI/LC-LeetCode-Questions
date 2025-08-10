f1c8b61634315768eda20bfe3d88ce8613ceecdc
WalukaUI <cwaluka@yahoo.com>
2025-08-10 13:46:44 -0500

    success but time exceeded

--- 6.ques.js
+++ 6.ques.js
@@ -1,6 +1,21 @@
 
 let nums = [1,2,3,4]
 function productExceptSelf(nums) {
-    
+    let newa=[]
+    for (let i = 0; i < nums.length; i++) {
+         let newfil = nums.filter((x,y)=> {
+            return x !== nums[i] || y!==i
+        }) 
+         let count = newfil[0]
+         for (let x = 1; x < newfil.length; x++) {
+              count = count * newfil[x]     
+         }
+         if (count === undefined) {
+            newa.push(0)
+         }else{
+            newa.push(count)
+         }
+    }
+    return newa
 };
 console.log(productExceptSelf(nums));
