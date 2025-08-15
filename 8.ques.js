f80a11e0963878c67fe13022bed1fafb02cece24
WalukaUI <cwaluka@yahoo.com>
2025-08-15 11:52:03 -0500

    ques 8 half done

--- 8.ques.js
+++ 8.ques.js
@@ -1,5 +1,19 @@
 let nums = [100,4,200,1,3,2]
 function longestConsecutive(nums) {
+    let newset = {1:[1]}
+    let newNums=nums.sort()
+    for (let i = 0; i < newNums.length; i++) {
+        let currentNum = newNums[i]
+        console.log(newset[currentNum-1]);
+        
+        if(newset[currentNum-1]){
+            newset[currentNum]=newset[currentNum-1]
+            delete newset[currentNum-1]
+            newset[currentNum].push(currentNum)
+        }
+        
+    }
+    console.log(newset);
     
 }
 
