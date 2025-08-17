882c28f8c53dfac0365fcc53ae1db0c31d7d7e7b
WalukaUI <cwaluka@yahoo.com>
2025-08-16 20:10:25 -0500

    corect answer

--- 5.ques.js
+++ 5.ques.js
@@ -1,24 +1,45 @@
 
 let nums = [1,1,4,1,2,2,3,7,7,7,7], k = 2
 
-function topKFrequent(num, kk) {
-    let newObj={}
 
-    for(x of num){
+function topKFrequent(nums, kk) {
+    let newObj={}
+    for(let x of nums){
         if(!newObj[x]){
             newObj[x]=[x]
         }else{
             newObj[x].push(x)
         }
     }
-    let uniqVals = Object.values(newObj).sort((a,b)=> b.length - a.length).slice(0, kk)
-    let newarray= []
-    for (let i = 0; i < uniqVals.length; i++) {
-        newarray.push(uniqVals[i][0])
-        
+    let valuesOfObj= Object.values(newObj).sort((a,b)=> b.length - a.length)
+    let count=0
+    let newa=[]
+    while(count< kk){
+        newa.push(valuesOfObj[count][0])
+        count++
     }
-    return newarray
-    
+    return newa
 }
+
+
+// function topKFrequent(num, kk) {
+//     let newObj={}
+
+//     for(x of num){
+//         if(!newObj[x]){
+//             newObj[x]=[x]
+//         }else{
+//             newObj[x].push(x)
+//         }
+//     }
+//     let uniqVals = Object.values(newObj).sort((a,b)=> b.length - a.length).slice(0, kk)
+//     let newarray= []
+//     for (let i = 0; i < uniqVals.length; i++) {
+//         newarray.push(uniqVals[i][0])
+        
+//     }
+//     return newarray
+    
+// }
 
 console.log(topKFrequent(nums, k));
--- 8.ques.js
+++ 8.ques.js
@@ -1,20 +1,37 @@
 let nums = [100,4,200,1,3,2]
 function longestConsecutive(nums) {
-    let newset = {1:[1]}
-    let newNums=nums.sort()
-    for (let i = 0; i < newNums.length; i++) {
-        let currentNum = newNums[i]
-        console.log(newset[currentNum-1]);
-        
-        if(newset[currentNum-1]){
-            newset[currentNum]=newset[currentNum-1]
-            delete newset[currentNum-1]
-            newset[currentNum].push(currentNum)
+    let numSet = new Set(nums)
+    var longest = 0
+    for(const x of nums){  
+        if(numSet.has(x-1) === false){
+            var length = 0
+            while (numSet.has(x+length)) {
+                 length++
+            }
+           longest = Math.max(length, longest)
         }
-        
     }
-    console.log(Object.);
+
+    return longest
+
     
 }
 
 console.log(longestConsecutive(nums));
+
+
+    // let newset = {1:[1]}
+    // let newNums=nums.sort()
+    // for (let i = 0; i < newNums.length; i++) {
+    //     let currentNum = newNums[i]
+    //     if(newset[currentNum-1]){
+    //         newset[currentNum]=newset[currentNum-1]
+    //         delete newset[currentNum-1]
+    //         newset[currentNum].push(currentNum)
+    //     }
+        
+    // }
+    // let keyasnum=Object.keys(newset).map(Number)
+    // let highestkey=Math.max(...keyasnum)
+    // return highestkey
+    
