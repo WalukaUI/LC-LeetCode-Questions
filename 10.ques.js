01bcbc837eb7b90ed20b540bd101d4012213cad9
WalukaUI <cwaluka@yahoo.com>
2025-08-20 20:32:02 -0500

    starting files of ques 10

---
+++ 10.ques.js
@@ -0,0 +1,15 @@
+
+let numbers = [2,7,11,15], target = 9
+function twoSum(numbers, target) {
+    let myset = new Set(numbers)
+    for (let i = 0; i < numbers.length; i++) {
+        
+        if(myset.has(target)){
+        
+        }
+        
+    }
+    
+
+}
+console.log(twoSum(navigator,t));
