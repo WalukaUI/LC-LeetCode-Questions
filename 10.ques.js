e6957470d58b1faa3e13c7b7da570440c677f32d
WalukaUI <cwaluka@yahoo.com>
2025-08-27 21:20:42 -0500

    correct ans for q 10

--- 10.ques.js
+++ 10.ques.js
@@ -1,15 +1,51 @@
 
-let numbers = [2,7,11,15], target = 9
-function twoSum(numbers, target) {
-    let myset = new Set(numbers)
-    for (let i = 0; i < numbers.length; i++) {
-        
-        if(myset.has(target)){
-        
-        }
-        
-    }
+let numbers = [1,3,4,5,7,10,11], target = 11
+
+function twoSum(sorted, tgt) {
+  let start=0
+  let end=sorted.length-1
+
+  while (start < end) {
+     let sum=sorted[start]+sorted[end]
+
+     if(sum>target){
+        end--
+     }else if(target> sum){
+        start++
+     }else{
+        return [sorted[start], sorted[end]]
+     }
+     
+  }
+
+  console.log(sorted[start], sorted[end]);
+  
     
 
 }
-console.log(twoSum(navigator,t));
+
+
+
+// function twoSum(numbers, target) {
+//     if(numbers.length <= 1){
+//         return numbers
+//     }
+//     var ans =[]
+//     let myset = new Set(numbers)
+//     for (let i = 0; i < numbers.length; i++) {
+//         let remainingNum = numbers[i]> target? numbers[i]-target : target - numbers[i]
+//         console.log(remainingNum);
+        
+//         if(myset.has(remainingNum)){
+//            let remainingNumIndex = numbers.indexOf(remainingNum)
+           
+//            ans = [i+1, remainingNumIndex+1]
+//            break
+//         }
+        
+//     }
+//     return ans.sort()
+    
+
+// }
+console.log(twoSum(numbers,target));
